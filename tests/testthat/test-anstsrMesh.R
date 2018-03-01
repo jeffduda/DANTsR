9d0895134d2921463d0cea37a2789aa4c333361d
Jeffrey Duda <jeff.duda@gmail.com>
2018-03-01 18:08:25 -0500

    ENH: more tests

--- tests/testthat/test-anstsrMesh.R
+++ tests/testthat/test-anstsrMesh.R
@@ -4,3 +4,24 @@ test_that("mesh can be created with new()", {
   x = new("antsrMesh")
   expect_true( class(x)=="antsrMesh")
 })
+
+test_that("mesh can be created with new()", {
+  x = new("antsrMesh", reserve=10)
+  expect_true( ( class(x)=="antsrMesh" ) &
+               ( antsrMeshGetNumberOfPoints(x)==10 ) )
+})
+
+
+# Test all parameter combos for the C++ templated parameters
+for ( d in c(2,3,4) ) {
+  for ( p in c("float", "double") ) {
+
+
+    test_that("mesh can be created with new()", {
+      x = new("antsrMesh", dimension=d, precision=p)
+      expect_true( ( class(x)=="antsrMesh" ) &
+                   ( x@dimension == d ) &
+                   ( x@precision == p ) )
+    })
+  }
+}
