43aed8462e55832e5b9934ecdbf96e5a2452bba4
Jeffrey Duda <jeff.duda@gmail.com>
2018-08-22 15:53:12 -0400

    Revert "ENH: point count filter"
    This reverts commit 90c58b2c991adb0be68ceb0e4b2e1321cf841dd0.

--- R/pointCountImage.R
+++ R/pointCountImage.R
@@ -5,23 +5,15 @@
 #' @export
 pointCountImage = function(points, img)
 {
+  countImg = img*0
 
-  countImg = NA
-  if ( class(points)=="antsrMesh") {
-    countImg = .Call("pointCountImage", points, img, package="DANTsR")
-  }
-  else {
-    countImg = img*0
+  idx = round(antsTransformPhysicalPointToIndex(img, points))
 
-    idx = round(antsTransformPhysicalPointToIndex(img, points))
-
-    for ( i in 1:dim(points)[1] ) {
-      #if ( prod(idx[i,]>0) & (prod(idx[i,]<=dim(img))) ) {
-      if ( indexIsInImage(img, idx[i,]) ) {
-        countImg[ idx[i,1], idx[i,2], idx[i,3] ] = countImg[idx[i,1], idx[i,2], idx[i,3]][1] + 1
-      }
+  for ( i in 1:dim(points)[1] ) {
+    #if ( prod(idx[i,]>0) & (prod(idx[i,]<=dim(img))) ) {
+    if ( indexIsInImage(img, idx[i,]) ) {
+      countImg[ idx[i,1], idx[i,2], idx[i,3] ] = countImg[idx[i,1], idx[i,2], idx[i,3]][1] + 1
     }
   }
-
   return(countImg)
 }
