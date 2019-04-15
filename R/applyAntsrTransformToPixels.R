8882f4f004077008547a4fb05422d4de5ea77cae
Jeffrey Duda <jeff.duda@gmail.com>
2019-04-15 16:12:14 -0400

    ENH: example tensor reorientation

--- R/applyAntsrTransformToPixels.R
+++ R/applyAntsrTransformToPixels.R
@@ -13,28 +13,39 @@
 #' # plot(img,img2)
 #' @export
 applyAntsrTransformToPixels <- function(transform, image, pixel.as="vector", in.place=FALSE) {
-  if ( typeof(transform) == "list")
+
+  if ( class(transform) == "antsrTransform")
   {
-    transform <- composeAntsrTransforms(transform)
+    transform <- list(transform)
   }
 
   pixel.as = tolower(pixel.as)
-  if ( pixel.as=="vector" | pixel.as=="covariantvector") {
-    if ( image@components != transform@dimension ) {
-      stop( "Dimensionality of transform must match number of components in image for pixel vector types" )
+
+  for ( tx in transform ) {
+    if ( pixel.as=="vector" | pixel.as=="covariantvector") {
+      if ( image@components != tx@dimension ) {
+        stop( "Dimensionality of transform must match number of components in image for pixel vector types" )
+      }
+    }
+    else if ( pixel.as=="diffusiontensor") {
+      if ( image@components != 6 ) {
+        stop("diffusiontensor pixels must have 6 componenets")
+      }
+      if ( tx@dimension != 3 ) {
+        stop("diffusion tensor pixels must have a 3D transform")
+      }
+    }
+    else {
+      stop("Invalid 'pixel.as' type")
+    }
+
+    if ( tx@type == "DisplacementFieldTransform" ) {
+      if ( tx@dimension != image@dimension ) {
+        stop("for displacement field transforms, field and image must have the same dimension")
+      }
     }
   }
-  else if ( pixel.as=="diffusiontensor") {
-    if ( image@components != 6 ) {
-      stop("diffusiontensor pixels must have 6 componenets")
-    }
-    if ( transform@dimension != 3 ) {
-      stop("diffusion tensor pixels must have a 3D transform")
-    }
-  }
-  else {
-    stop("Invalid 'pixel.as' type")
-  }
+
 
   outImg = NA
   if ( in.place ) {
@@ -44,6 +55,15 @@ applyAntsrTransformToPixels <- function(transform, image, pixel.as="vector", in.
     outImg = antsImageClone(image)
   }
 
-  .Call("antsrTransform_TransformPixels", transform, outImg, pixel.as, PACKAGE = "DANTsR")
+  for ( t in rev(transform) ) {
+    .Call("antsrTransform_TransformPixels", t, outImg, pixel.as, PACKAGE="DANTsR")
+  }
+
+
+  if ( in.place ) {
+    return(NA)
+  }
+
   return(outImg)
+
 }
